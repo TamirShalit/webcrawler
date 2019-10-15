4a0d5731df03e6decca9ce575d7d4cc151f5fc60
TamirShalit <shalit.tamir@gmail.com>
2019-10-15 15:34:59 +0300

    Add basic setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='newscollector',
+    version='0.1',
+    packages=find_packages(exclude=['tests*']),
+    install_requires=[],
+    extras_require={'test': ['pytest']},
+    author='Tamir Shalit',
+    author_email='shalit.tamir@gmail.com',
+    description='Download, extraction and analysis of articles from '
+)
