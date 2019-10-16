a40f59289bb3d9b7aa2318c0c622f060959fb961
TamirShalit <shalit.tamir@gmail.com>
2019-10-16 22:27:06 +0300

    Implement method

--- newscollector/news_downloader.py
+++ newscollector/news_downloader.py
@@ -1,8 +1,10 @@
 import abc
 import os
+import re
+import time
 import urllib
 import urlparse
-from datetime import datetime
+import datetime
 
 import requests
 from bs4 import BeautifulSoup
@@ -24,7 +26,7 @@ class NewsDownloader(object):
         """Download news from the website to the download directory."""
         pass
 
-    def get_page_source(self):
+    def load_page_source(self):
         return requests.get(self.DOWNLOAD_URL).text
 
 
@@ -40,9 +42,10 @@ class SeleniumNewsDownloader(NewsDownloader):
         super(SeleniumNewsDownloader, self).__init__(download_directory)
         self._web_driver = driver_type(web_driver_location)
 
-    def get_page_source(self):
-        self._web_driver.get(self.DOWNLOAD_URL)
-        source = self._web_driver.page_source
+    def load_page_source(self):
+        with self._web_driver:
+            self._web_driver.get(self.DOWNLOAD_URL)
+            source = self._web_driver.page_source
         return source
 
 
@@ -51,7 +54,7 @@ class BBCNewsDownloader(NewsDownloader):
     DOWNLOAD_URL = 'http://bbc.com'
 
     def download_news(self):
-        soup = BeautifulSoup(self.get_page_source(), self.WEB_SCRAPPING_PARSER)
+        soup = BeautifulSoup(self.load_page_source(), self.WEB_SCRAPPING_PARSER)
         all_article_tags = soup.find_all(name='a', attrs={'class': 'block-link__overlay-link'})
         for tag in all_article_tags:
             self._download_article_from_html_tag(tag)
@@ -85,20 +88,59 @@ class BenGurionAirportScheduleDownloader(SeleniumNewsDownloader):
     Downloads real-time schedule of flights from Ben-Gurion airport.
     """
     DOWNLOAD_URL = 'http://www.iaa.gov.il/he-IL/airports/BenGurion/Pages/OnlineFlights.aspx'
+    SECONDS_BETWEEN_SCHEDULE_UPDATES = 60 * 5
+
+    _HOUR_REGEX = r'([01]\d|2[0-3]):[0-5]\d'
+
+    def __init__(self, download_directory, web_driver_location, driver_type=webdriver.Chrome):
+        super(BenGurionAirportScheduleDownloader, self).__init__(download_directory,
+                                                                 web_driver_location, driver_type)
+        self._last_update_time = None
+        self._last_loaded_page_source = ''
 
     def download_news(self):
-        page_source = self.get_page_source()
-        self._download_news(page_source)
+        self._last_loaded_page_source = self.load_page_source()
+        self._write_schedule_file(self._last_loaded_page_source)
+
+    def download_news_perpetually(self):
+        self.download_news()
+        self._last_update_time = self._extract_schedule_update_time(self._last_loaded_page_source)
+        time_passed_since_last_update = datetime.datetime.now() - self._last_update_time
+        expected_seconds_to_next_update = (datetime.timedelta(
+            seconds=self.SECONDS_BETWEEN_SCHEDULE_UPDATES) -
+                                           time_passed_since_last_update).total_seconds()
+        time.sleep(expected_seconds_to_next_update)
+
+        while self._should_download_schedule():
+            self._write_schedule_file(self._last_loaded_page_source)
+            time.sleep(self.SECONDS_BETWEEN_SCHEDULE_UPDATES)
+
+    def _should_download_schedule(self):
+        self._last_loaded_page_source = self.load_page_source()
+        schedule_update_time = self._extract_schedule_update_time(self._last_loaded_page_source)
+        if schedule_update_time > self._last_update_time:
+            self._last_update_time = schedule_update_time
+            return True
+        return False
+
+    def _extract_schedule_update_time(self, page_source):
+        current_datetime = datetime.datetime.now()
+        year, month, day = current_datetime.year, current_datetime.month, current_datetime.day
+        soup = BeautifulSoup(page_source, self.WEB_SCRAPPING_PARSER)
+        last_update_tag = soup.find(id='ctl00_rptIncomingFlights_ctl00_pInformationStatusMessage')
+        last_update_hour_string = re.search(self._HOUR_REGEX, last_update_tag.text).group(0)
+        hour, seconds = map(int, last_update_hour_string.split(':'))
+        return datetime.datetime(year, month, day, hour, seconds)
 
     @staticmethod
-    def _download_news(page_source):
+    def _write_schedule_file(page_source):
         download_file_name = 'flights_schedule_{current_time}.html'.format(
-            current_time=datetime.now())
+            current_time=datetime.datetime.now())
         with open(download_file_name, 'w') as download_file:
-            download_file.write(page_source)
+            download_file.write(page_source.encode('utf-8'))
 
 
 if __name__ == '__main__':
     # BBCNewsDownloader('/Users/tamir/temp').download_news()
     BenGurionAirportScheduleDownloader('/Users/tamir/temp',
-                                       '/Users/tamir/Downloads/chromedriver').download_news()
+                                       '/Users/tamir/Downloads/chromedriver').download_news_perpetually()
