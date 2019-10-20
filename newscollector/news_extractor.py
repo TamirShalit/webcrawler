4f4682403acd243bc51bf541df940c11666bef65
TamirShalit <shalit.tamir@gmail.com>
2019-10-20 22:08:03 +0300

    Implement 'BenGurionFlightSchedule'

--- newscollector/news_extractor.py
+++ newscollector/news_extractor.py
@@ -1,13 +1,10 @@
 import abc
 import collections
-import json
-import os
-from datetime import datetime
 
 from bs4 import BeautifulSoup
 
 from newscollector import common
-from newscollector.raw_news import BBCRawArticle
+from newscollector.raw_news import BBCRawArticle, BenGurionFlightSchedule
 
 FlightData = collections.namedtuple('FlightData',
                                     ['number', 'flight_from', 'planned_time', 'updated_time',
@@ -18,10 +15,9 @@ class NewsExtractor(object):
     """Extracts relevant raw data from downloaded news files."""
     __metaclass__ = abc.ABCMeta
 
-    def __init__(self, news_file_path, output_directory):
+    def __init__(self, news_file_path):
         self.news_file_path = news_file_path
         self._news_file_content = ''
-        self.output_directory = output_directory
 
     @property
     def news_file_content(self):
@@ -64,39 +60,23 @@ class BenGurionAirportScheduleExtractor(NewsExtractor):
 
     def extract_raw_news(self):
         soup = BeautifulSoup(self.news_file_content, common.WEB_SCRAPPING_PARSER)
-        all_flights_tags = soup.find_all(name='tr', attrs={
-            'class': ['odd', 'even']})[1:]
-        for tag in all_flights_tags:
-            flight_data = self._extract_flight_data(tag)
-
-            flight_data_output_path = os.path.join(self.output_directory,
-                                                   '{flight_number} {current_time}.json'.format(
-                                                       flight_number=flight_data.number,
-                                                       current_time=datetime.now()))
-            with open(flight_data_output_path, 'w') as flight_data_output_file:
-                json.dump(flight_data._asdict(), flight_data_output_file, indent=2)
+        all_flights_tags = soup.find_all(name='tr', attrs={'class': ['odd', 'even']})[1:]
+        return [self._extract_flight_schedule(tag) for tag in all_flights_tags]
 
     @staticmethod
-    def _extract_flight_data(flight_html_row):
+    def _extract_flight_schedule(flight_html_row):
         flight_number = flight_html_row.find(name='td', attrs={'class': 'FlightNum'}).text.strip()
         flight_from = flight_html_row.find(name='td', attrs={'class': 'FlightFrom'}).find(
             'span').text.strip()
         planned_time = flight_html_row.find(name='td', attrs={'class': 'FlightTime'}).text.strip()
         updated_time = flight_html_row.find(name='td', attrs={'class': 'finalTime'}).text.strip()
-        local_terminal = flight_html_row.find(name='td',
-                                              attrs={'class': 'localTerminal'}).text.strip()
+        local_terminal = int(flight_html_row.find(name='td', attrs={'class': 'localTerminal'}).text)
         status = flight_html_row.find(name='td', attrs={'class': 'status'}).find('div').text.strip()
-        return FlightData(number=flight_number, flight_from=flight_from, planned_time=planned_time,
-                          updated_time=updated_time, terminal=local_terminal, status=status)
+        return BenGurionFlightSchedule(flight_number, flight_from, planned_time, updated_time,
+                                       local_terminal, status)
 
 
 if __name__ == '__main__':
-    raw_news = BBCNewsExtractor(
-        "/Users/tamir/PycharmProjects/newscollector/newscollector/Why won't Democrats vote to authorise impeachment_ - BBC News.htm",
-        '/Users/tamir/PycharmProjects/newscollector/newscollector/output').extract_raw_news()[0]
-    print raw_news.header
-    print raw_news.introduction
-    print raw_news.paragraphs
-    # BenGurionAirportScheduleExtractor(
-    #     '/Users/tamir/PycharmProjects/newscollector/newscollector/flights_schedule_2019-10-16 23:48:13.490611.html',
-    #     '/Users/tamir/PycharmProjects/newscollector/newscollector/output').extract_raw_news()
+    all_schedules = BenGurionAirportScheduleExtractor(
+        '/Users/tamir/PycharmProjects/newscollector/newscollector/flights_schedule_2019-10-16 23:48:13.490611.html').extract_raw_news()
+    print all_schedules[0].to_dict()
--- newscollector/raw_news.py
+++ newscollector/raw_news.py
@@ -1,4 +1,5 @@
 import abc
+import json
 import os
 
 
@@ -60,3 +61,66 @@ class BBCRawArticle(RawNews):
     @property
     def paragraphs(self):
         return self._paragraphs
+
+
+class BenGurionFlightSchedule(RawNews):
+    FLIGHT_NUMBER_FIELD = 'number'
+    FLIGHT_FROM_FIELD = 'from'
+    PLANNED_TIME_FIELD = 'planned_time'
+    UPDATED_TIME_FIELD = 'updated_time'
+    TERMINAL_FIELD = 'terminal'
+    STATUS_FIELD = 'status'
+
+    def __init__(self, flight_number, flight_from, planned_time, updated_time, terminal, status):
+        self._flight_number = flight_number
+        self._flight_from = flight_from
+        self._planned_time = planned_time
+        self._updated_time = updated_time
+        self._terminal = terminal
+        self._status = status
+
+    def dump(self, file_path):
+        with open(file_path, 'w') as output_file:
+            json.dump(self.to_dict(), output_file, indent=2)
+
+    @classmethod
+    def load(cls, file_path):
+        with open(file_path) as flight_schedule_file:
+            return cls.from_dict(json.load(flight_schedule_file))
+
+    @property
+    def flight_number(self):
+        return self._flight_number
+
+    @property
+    def flight_from(self):
+        return self._flight_from
+
+    @property
+    def planned_time(self):
+        return self._planned_time
+
+    @property
+    def updated_time(self):
+        return self._updated_time
+
+    @property
+    def terminal(self):
+        return self._terminal
+
+    @property
+    def status(self):
+        return self._status
+
+    def to_dict(self):
+        return {self.FLIGHT_NUMBER_FIELD: self.flight_number,
+                self.FLIGHT_FROM_FIELD: self.flight_from,
+                self.PLANNED_TIME_FIELD: self.planned_time,
+                self.UPDATED_TIME_FIELD: self.updated_time,
+                self.STATUS_FIELD: self.status}
+
+    @classmethod
+    def from_dict(cls, flight_dict):
+        return cls(flight_dict[cls.FLIGHT_NUMBER_FIELD], flight_dict[cls.FLIGHT_FROM_FIELD],
+                   flight_dict[cls.PLANNED_TIME_FIELD], flight_dict[cls.UPDATED_TIME_FIELD],
+                   flight_dict[cls.TERMINAL_FIELD], flight_dict[cls.STATUS_FIELD])
